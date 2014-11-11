d36e40c21eb077f62c1e9db2d956768db7602202
Romuald du Song <romuald.dusong@deveryware.com>
2014-11-11 21:07:33 +0100

    increase counter on each request

--- src/hello_handler.erl
+++ src/hello_handler.erl
@@ -6,4 +6,5 @@ init(Req, Opts) ->
     Req2 = cowboy_req:reply(200, [
             {<<"content-type">>, <<"text/plain">>}
         ], <<"Hello world!">>, Req),
+    first_cowboy_stats:request(),
     {ok, Req2, Opts}.
