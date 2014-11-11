8f42d79a1a1495a03b073f2d03a50e931e778fa0
Romuald du Song <romuald.dusong@deveryware.com>
2014-11-11 21:07:05 +0100

    trace application status via alarm_handler, fetch pool size from env

--- src/first_cowboy_app.erl
+++ src/first_cowboy_app.erl
@@ -4,15 +4,36 @@
 -export([start/2]).
 -export([stop/1]).
 
+-define(HANDLER_POOL_SIZE,100).
+
 start(_Type, _Args) ->
-    application:start(folsomite),
+    HandlerPoolSize = get_app_env(handler_pool_size, ?HANDLER_POOL_SIZE),
     Dispatch = cowboy_router:compile([
             {'_', [{"/", hello_handler, []}]}
         ]),
-    cowboy:start_http(my_http_listener, 10, [{port, 8080}],
+    cowboy:start_http(my_http_listener, HandlerPoolSize, [{port, 8080}],
         [{env, [{dispatch, Dispatch}]}]
     ),
-    first_cowboy_sup:start_link().
+    case first_cowboy_sup:start_link() of
+        {ok, Pid} ->
+            alarm_handler:clear_alarm({application_stopped, first_cowboy}),
+            {ok, Pid};
+        Error ->
+            alarm_handler:set_alarm({{application_stopped, first_cowboy}, []}),
+            Error
+    end.
 
 stop(_State) ->
+    alarm_handler:set_alarm({{application_stopped, first_cowboy}, []}),
     ok.
+
+get_app_env(Opt, Default) ->
+    {ok, Application} = application:get_application(),
+    case application:get_env(Application, Opt) of
+        {ok, Val} -> Val;
+        _ ->
+            case init:get_argument(Opt) of
+                {ok, [[Val | _]]} -> Val;
+                error       -> Default
+            end
+    end.
